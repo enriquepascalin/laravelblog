5471275dec40c6553e4bdcce1da88584f022e604
Enrique Pascalin <erparom@gmail.com>
2025-05-28 09:30:16 -0600

    new suggestions

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,5 @@
 return [
     App\Providers\AnalyticsServiceProvider::class,
     App\Providers\AppServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\CommentController;
 
 Route::middleware('auth:sanctum')->group(function () {
   Route::apiResource('posts', PostController::class);
-  Route::apiResource('comments', CommentController::class)->except(['index', 'show']);
+  Route::delete('/posts/{post}', [PostController::class, 'destroy'])->middleware('admin');
 });
